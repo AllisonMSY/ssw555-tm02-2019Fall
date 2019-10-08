83517bc8ab7447f9b939ceb6cf219c7da1f632a4
zfz <zhoufangzhang@gmail.com>
2019-10-08 22:30:21 +0000

    Unit test story 04

--- test.py
+++ test.py
@@ -25,5 +25,12 @@ class test(unittest.TestCase):
                             "", "")
         self.assertEqual(p.birth_before_death()[0], False)
 
+    def test_story04(self):
+        f = project4.Family("F04T")
+        f.Married, f.Divorced = "4 MAR 2000", "4 MAR 2010"
+        self.assertEqual(f.marriage_before_divorce(), True)
+        f.Married, f.Divorced = "4 MAR 2010", "4 MAR 2000"
+        self.assertEqual(f.marriage_before_divorce(), False)
+
 if __name__ == '__main__':
     unittest.main()
