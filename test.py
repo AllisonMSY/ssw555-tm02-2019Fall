616b846e0e58d4806246d0d07bb83df7d86ea725
zeyuh <zh135@scarletmail.rutgers.edu>
2019-10-08 18:17:41 -0400

    123

--- test.py
+++ test.py
@@ -3,10 +3,10 @@ import project4
 
 class test(unittest.TestCase):
 
-    def story08(self):
+    def story07(self):
         p1 = project4.Person(I01, dadasaf,M,'DATE 2 SEP 1741','DATE 21 DEC 1973','','')
         p2 = project4.Person(I02,dadva,F,'DATE 21 DEC 1943','','','')
-        self.assertFalse(p1.less_than_150())
+        self.assertFalse(p1.less_than_150()[0])
         self.assertTrue(p2.less_than_150())
 
 
